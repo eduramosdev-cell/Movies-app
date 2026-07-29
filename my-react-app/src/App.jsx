5706ebed6ff3e6c8c9831699904df7ada2968cd5
Edu-Ra <edugeselino@gmail.com>
2026-07-29 17:20:08 -0300

    Project start. Dependencies installed

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-3xl font-bold text-blue-600 underline">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
