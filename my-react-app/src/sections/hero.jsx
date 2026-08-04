69c09fa42b7ae4c57424ae9fbfc762f2d18a7b65
Eduardo Ramos <258696090+Edu-Ra@users.noreply.github.com>
2026-08-03 23:23:23 -0300

    Hero updated (mock)

--- my-react-app/src/sections/hero.jsx
+++ my-react-app/src/sections/hero.jsx
@@ -1,11 +1,27 @@
-import { MockApi } from "../MockApi/Mock.jsx";
-import poster from "../MockApi/MoviePoster.jpg";
+import poster from "../MockApi/SpidermanPoster.jpg";
 
 export const Hero = () => {
+
+    const MockApi =  {
+    rating: 8.5,
+    description: "A thrilling adventure of a young hero who must save the world from impending doom. With breathtaking visuals and a gripping storyline, this movie is a must-watch for fans of action and fantasy.",
+    title: "The Hero's Journey",
+    releaseDate: "2023-05-15",
+}
+
     return <div className="relative w-full bg-background flex flex-col items-center justify-center overflow-hidden">
         {/* Hero img */}
-        <div className="w-full -mt-16">
+        <div className="w-full z-0 absolute top-0 left-0">
             <img src={poster} alt="Hero" className="w-full h-auto object-cover" />
+        </div>
+        {/* Show Info */}
+        <div className="relative w-full h-screen bg-linear-40 from-black via-black/40 to-transparent flex flex-col items-start justify-center overflow-hidden">
+        <div className="z-10 p-8 text-white flex flex-col items-start">
+            <h1 className="text-5xl font-bold mb-4 pt-16">{MockApi.title}</h1>
+            <p className="text-xl mb-2">{MockApi.rating}/10</p>
+            <p className="text-lg mb-4 max-w-md">{MockApi.description}</p>
+            <p className="text-md text-gray-300">{MockApi.releaseDate.slice(0, 4)}</p>
+        </div>
         </div>
     </div>
 }
